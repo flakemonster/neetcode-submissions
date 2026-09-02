4362d6f462d8402390cc1bb5d0d6990f0c29b2ba
flakemonster <152272157+flakemonster@users.noreply.github.com>
2026-09-02 22:06:54 +0100

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.java
@@ -0,0 +1,48 @@
+class TrieNode {
+    HashMap<Character, TrieNode> children = new HashMap<>();
+    boolean endOfWord = false;
+}
+
+class PrefixTree {
+    TrieNode root;
+    public PrefixTree() {
+         root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode cur = root;
+        //System.out.println("insert "+word);
+        for(char c: word.toCharArray()) {
+            //System.out.println("inserting "+c);
+            if(!cur.children.containsKey(c)) {
+                cur.children.put(c, new TrieNode());
+            }
+            cur = cur.children.get(c);
+        }
+
+        cur.endOfWord = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode cur = root;
+
+        for(char c: word.toCharArray()) {
+            if(!cur.children.containsKey(c)) {
+                return false;
+            }
+            cur = cur.children.get(c);
+        }
+        return cur.endOfWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode cur = root;
+        for(char c: prefix.toCharArray()) {
+            if(!cur.children.containsKey(c)) {
+                return false;
+            }
+            cur = cur.children.get(c);
+        }
+        return true;
+    }
+}
